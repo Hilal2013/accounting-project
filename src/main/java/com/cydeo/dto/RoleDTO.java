ec01aa4115d08556b2e6ae46c0b98f2bded7baef
Ladi_Cydeo <ladikhachidze@gmail.com>
2023-06-26 19:15:14 -0400

    Updated Role Entity & DTO

--- src/main/java/com/cydeo/dto/RoleDTO.java
+++ src/main/java/com/cydeo/dto/RoleDTO.java
@@ -1,13 +1,12 @@
 package com.cydeo.dto;
 
 
-import com.cydeo.entity.BaseEntity;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+
 
 @Data
 @NoArgsConstructor
