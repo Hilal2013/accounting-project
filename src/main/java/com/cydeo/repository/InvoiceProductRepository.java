2758beb40ff1c78de15e2b3b7c27ad716b4727aa
Zehra <busraebrar2014@hotmail.com>
2023-06-27 14:18:02 -0400

    update

--- src/main/java/com/cydeo/repository/InvoiceProductRepository.java
+++ src/main/java/com/cydeo/repository/InvoiceProductRepository.java
@@ -1,4 +1,7 @@
 package com.cydeo.repository;
 
-public interface InvoiceProductRepository {
+import com.cydeo.entity.InvoiceProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvoiceProductRepository extends JpaRepository<InvoiceProduct,Long> {
 }
