b814a62fe600d6aff8238127f363c8f5ce26a9ba
Yul28Vas <ygvasilenko@gmail.com>
2023-06-27 11:18:04 -0400

    CategoryDTO

---
+++ src/main/java/com/cydeo/dto/CategoryDTO.java
@@ -0,0 +1,19 @@
+package com.cydeo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CategoryDTO {
+
+    private Long id;
+    private String description;
+    private CompanyDTO company;
+    private boolean hasProduct;
+
+}
