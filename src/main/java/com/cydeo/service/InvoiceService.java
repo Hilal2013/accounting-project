e5fa6920ccb212fa144b1509fb5b5e0898933c2c
Zehra <busraebrar2014@hotmail.com>
2023-06-27 14:22:32 -0400

    create InvoiceService

--- src/main/java/com/cydeo/service/InvoiceService.java
+++ src/main/java/com/cydeo/service/InvoiceService.java
@@ -1,4 +1,16 @@
 package com.cydeo.service;
 
+import com.cydeo.dto.InvoiceDTO;
+import com.cydeo.entity.Invoice;
+
+import java.util.List;
+
 public interface InvoiceService {
+    InvoiceDTO createInvoice();
+    InvoiceDTO updateInvoice();
+    List<InvoiceDTO> listAllInvoice();
+
+    void deleteInvoice(String invoiceNo);
+
 }
+
--- src/main/java/com/cydeo/service/impl/InvoiceServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/InvoiceServiceImpl.java
@@ -1,8 +1,30 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.dto.InvoiceDTO;
 import com.cydeo.service.InvoiceService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class InvoiceServiceImpl implements InvoiceService {
+    @Override
+    public InvoiceDTO createInvoice() {
+        return null;
+    }
+
+    @Override
+    public InvoiceDTO updateInvoice() {
+        return null;
+    }
+
+    @Override
+    public List<InvoiceDTO> listAllInvoice() {
+        return null;
+    }
+
+    @Override
+    public void deleteInvoice(String invoiceNo) {
+
+    }
 }
