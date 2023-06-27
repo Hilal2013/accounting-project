9a7ccc2e07ba4ea0aa292c14fb02cf67ba0ee715
Ladi_Cydeo <ladikhachidze@gmail.com>
2023-06-27 11:33:15 -0400

    TECH-13 Create "findByUsername" method

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -2,11 +2,13 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.User;
+import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.UserRepository;
 import com.cydeo.service.UserService;
 
 public class UserServiceImpl implements UserService {
 
+    private MapperUtil mapperUtil;
     private UserRepository userRepository;
 
     @Override
@@ -14,7 +16,7 @@ public class UserServiceImpl implements UserService {
 
         User user = userRepository.findByUserName(userName);
 
-        return   null;  //userMapper.convertToDto(user);
+        return   mapperUtil.convert(user, new UserDTO());
     }
 
 
