8f248644d76cb541395fad42e28412cb1d44d6eb
Hilal2013 <hilalozkul@gmail.com>
2023-06-27 09:28:51 -0400

    updated

--- src/main/java/com/cydeo/enums/ClientVendorType.java
+++ src/main/java/com/cydeo/enums/ClientVendorType.java
@@ -1,7 +1,7 @@
 package com.cydeo.enums;
 
 public enum ClientVendorType {
-    VENDOR("Vendor"),client("Client");
+    VENDOR("Vendor"),CLIENT("Client");
     private final String value;
 
     public String getValue() {
