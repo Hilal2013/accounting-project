c9d93d3dc5c9c024999da849d969265b7a4e254f
Yul28Vas <ygvasilenko@gmail.com>
2023-06-29 11:04:38 -0400

    Created CategoryService interface

---
+++ src/main/java/com/cydeo/service/CategoryService.java
@@ -0,0 +1,10 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.CategoryDTO;
+import java.util.List;
+
+public interface CategoryService {
+    CategoryDTO findById(Long id);
+    List<CategoryDTO> getCategoryList();
+
+}
