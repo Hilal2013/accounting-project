ae41d67f156db5e85c69c6b8323b1f9b62f55643
Hilal2013 <hilalozkul@gmail.com>
2023-07-02 16:21:15 -0400

    updated

--- src/main/java/com/cydeo/dto/CompanyDTO.java
+++ src/main/java/com/cydeo/dto/CompanyDTO.java
@@ -23,14 +23,11 @@ public class CompanyDTO {
     @NotBlank(message = "Title is a required field.")
     @Size(min = 2, max = 100, message = "Title should be 2-100 characters long.")
     private String title;
-    @NotBlank(message = "Phone Number is a required field and may be in any valid phone number format.")
     @Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$"
             + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"
             + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$",
             message = "Phone Number is required field and may be in any valid phone number format.")
     private String phone;
-    @NotBlank(message = "Website is a required field.")
-    @NotBlank(message = "Website is a required field")
     @Pattern(regexp = "^(http:\\/\\/|https:\\/\\/)?(www\\.)?[a-zA-Z0-9-_\\.]+" +
             "\\.[a-zA-Z]+(:\\d+)?(\\/[a-zA-Z\\d\\.\\-_]*)*[a-zA-Z.!@#$%&=-_'\":,.?\\d*)(]*$"
             , message = "Website should have a valid format.")
