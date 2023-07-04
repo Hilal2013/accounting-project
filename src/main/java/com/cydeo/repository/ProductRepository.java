0ad8287cdfbe17e17612ab9d15dfd4c043fdfd4f
Nahom <nahomweldemicael1@gmail.com>
2023-07-04 11:36:47 -0400

    listing based on company

--- src/main/java/com/cydeo/repository/ProductRepository.java
+++ src/main/java/com/cydeo/repository/ProductRepository.java
@@ -11,8 +11,10 @@ import java.util.Optional;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAll();
-    List<Product> findAllByIsDeletedOrderByCategoryAscNameAsc(boolean logic);
-//    List<Product> findAllByCompany(Company company);
+//    List<Product> findAllByIsDeletedOrderByCategoryAscNameAsc(boolean logic);
+
+
+    List<Product> findAllByCategory_Company_IdOrderByCategoryAscNameAsc(Long id);
 
 
 }
