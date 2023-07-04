a00484edfa62d8567820233e7bd26c96b90698d9
Yul28Vas <ygvasilenko@gmail.com>
2023-07-04 12:52:48 -0400

    CategoryServiceImpl_line77_listAllCategoriesByCompany

--- src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
@@ -1,12 +1,16 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.CategoryDTO;
+import com.cydeo.dto.CompanyDTO;
 import com.cydeo.entity.Category;
+import com.cydeo.entity.Company;
 import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.CategoryRepository;
 import com.cydeo.service.CategoryService;
+import com.cydeo.service.CompanyService;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -17,10 +21,12 @@ public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
     private final MapperUtil mapperUtil;
+    private final CompanyService companyService;
 
-    public CategoryServiceImpl(CategoryRepository categoryRepository, MapperUtil mapperUtil) {
+    public CategoryServiceImpl(CategoryRepository categoryRepository, MapperUtil mapperUtil, CompanyService companyService) {
         this.categoryRepository = categoryRepository;
         this.mapperUtil = mapperUtil;
+        this.companyService = companyService;
     }
 
     @Override
@@ -64,6 +70,17 @@ public class CategoryServiceImpl implements CategoryService {
         categoryInDb.setIsDeleted(true);
         categoryInDb.setDescription(categoryInDb.getDescription()+"-"+categoryInDb.getId());
         categoryRepository.save(categoryInDb);
+    }
+
+
+    @Override
+    public List<CategoryDTO> listAllCategoriesByCompany(){
+        List<Category> categoryList = categoryRepository
+                .getAllProductsOrderByCompanyCompanyAsc(companyService.getCompanyDTOByLoggedInUser().getId());
+        return categoryList.stream()
+                .sorted(Comparator.comparing(Category::getDescription))
+                .map(category -> mapperUtil.convert(category, new CategoryDTO()))
+                .collect(Collectors.toList());
     }
 
 
