0efa4d06146392c23cc578e22cfd3d05723d8226
Yul28Vas <ygvasilenko@gmail.com>
2023-07-10 19:27:29 -0400

    CategoryDTO_description_NotBlank_NotNull_Size

--- src/main/java/com/cydeo/dto/CategoryDTO.java
+++ src/main/java/com/cydeo/dto/CategoryDTO.java
@@ -5,6 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -12,7 +16,12 @@ import lombok.Setter;
 public class CategoryDTO {
 
     private Long id;
+
+    @NotBlank(message="Description is a required field.")
+    @NotNull
+    @Size(max=100, min=2, message = "Description should have 2-100 characters long.")
     private String description;
+
     private CompanyDTO company;
     private boolean hasProduct;
 
